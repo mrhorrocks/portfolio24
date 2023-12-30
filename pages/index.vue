8a0e021bb41faa6b85f3b954be272dbd91109cf2
Mark Horrocks <mr.horrocks@gmail.com>
2023-12-30 23:59:06 +0000

    home page silder scaffold added

--- pages/index.vue
+++ pages/index.vue
@@ -16,11 +16,136 @@ if (location.href.indexOf("#invite_token") !== -1) {
 <template>
   <section>
     <div class="container">
-      <h1>HOME</h1>
-      <h2>HOME</h2>
-      <h3>HOME</h3>
-      <h4>HOME</h4>
-      <h5>HOME</h5>
+      <div class="slide-wrapper">
+        <input type="radio" name="slide" id="c1" checked />
+        <label for="c1" class="card">
+          <div class="row">
+            <div class="icon">1</div>
+            <div class="description">
+              <h4>Winter</h4>
+              <p>Winter has so much to offer - creative activities</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c2" />
+        <label for="c2" class="card">
+          <div class="row">
+            <div class="icon">2</div>
+            <div class="description">
+              <h4>Digital Technology</h4>
+              <p>Gets better every day - stay tuned</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c3" />
+        <label for="c3" class="card">
+          <div class="row">
+            <div class="icon">3</div>
+            <div class="description">
+              <h4>Globalization</h4>
+              <p>Help people all over the world</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c4" />
+        <label for="c4" class="card">
+          <div class="row">
+            <div class="icon">4</div>
+            <div class="description">
+              <h4>New technologies</h4>
+              <p>Space engineering becomes more and more advanced</p>
+            </div>
+          </div>
+        </label>
+      </div>
     </div>
   </section>
 </template>
+
+<style>
+.slide-wrapper {
+  height: 400px;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+}
+
+.card {
+  width: 80px;
+  background-size: cover;
+  background-color: white;
+  cursor: pointer;
+  overflow: hidden;
+  border-radius: 1rem;
+  margin: 0 10px;
+  display: flex;
+  align-items: flex-end;
+  transition: 0.6s cubic-bezier(0.28, -0.03, 0, 0.99);
+  box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.8);
+}
+
+.card > .row {
+  color: white;
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.card > .row > .icon {
+  background: #223;
+  color: white;
+  border-radius: 50%;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+}
+
+.card > .row > .description {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  overflow: hidden;
+  height: 80px;
+  width: 520px;
+  opacity: 0;
+  transform: translateY(30px);
+  transition-delay: 0.3s;
+  transition: all 0.3s ease;
+}
+
+.description p {
+  color: #b0b0ba;
+  padding-top: 5px;
+}
+
+.description h4 {
+  text-transform: uppercase;
+}
+
+input {
+  display: none;
+}
+
+input:checked + label {
+  width: 600px;
+}
+
+input:checked + label .description {
+  opacity: 1 !important;
+  transform: translateY(0) !important;
+}
+
+.card[for="c1"] {
+  background-image: url("./img1.jpeg");
+}
+.card[for="c2"] {
+  background-image: url("./img2.jpeg");
+}
+.card[for="c3"] {
+  background-image: url("./img3.jpeg");
+}
+.card[for="c4"] {
+  background-image: url("./img4.jpeg");
+}
+</style>
