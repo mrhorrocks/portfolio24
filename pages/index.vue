1ef5387fecaef8ddb2df1ce2b7f4ad46c9a07520
Mark Horrocks <mr.horrocks@gmail.com>
2024-02-06 23:04:12 +0000

    1st grid scaffold

--- pages/index.vue
+++ pages/index.vue
@@ -15,69 +15,34 @@ if (location.href.indexOf("#invite_token") !== -1) {
 
 <template>
   <section>
-    <div class="container md:grid md:cols-3">
-      <!-- Headline -->
+    <div class="container md:grid md:cols-2">
+      <div class="col-span-1">
+        <!-- Headline -->
+        <h1 class="site-headline">
+          <span>Front-End</span> <span>Developer</span>
+        </h1>
+        <h2 class="sub-text">
+          <ul>
+            <li>I build websites with Nuxt, Netlifty & Decap CMS.</li>
+            <li>Based in North- West England.</li>
+          </ul>
+        </h2>
+
+        <!-- <div class="tech-logos">
+          <img src="/img/homepage/tech_logos/Html5.png" alt="Html5" />
+          <img src="/img/homepage/tech_logos/CSS3.png" alt="CSS3" />
+          <img src="/img/homepage/tech_logos/Sass.png" alt="Sass" />
+          <img src="/img/homepage/tech_logos/Nuxt.png" alt="Nuxt" />
+          <img src="/img/homepage/tech_logos/JS.png" alt="JS" />
+          <img src="/img/homepage/tech_logos/Decap.png" alt="Decap" />
+        </div> -->
+      </div>
 
       <div class="col-span-1">
-        <h1 class="site-headline">
-          This is my Portfolio.
-          <br />
-          <span>Built with </span><span>Nuxt, Git</span> &
-          <span>Netlify</span>
-        </h1>
-      </div>
-      <!-- Slides -->
-      <div class="slide-wrapper col-span-2">
-        <input type="radio" name="slide" id="c1" checked />
-        <label for="c1" class="card">
-          <div class="row">
-            <div class="icon">1</div>
-            <div class="description">
-              <h4>Big Robot</h4>
-              <p>Blender, Flash, HTML, CSS</p>
-              <NuxtLink to="/blog/2024-01-18-graphics-2008/"
-                >KEEP READING >></NuxtLink
-              >
-            </div>
-          </div>
-        </label>
-
-        <input type="radio" name="slide" id="c2" />
-        <label for="c2" class="card">
-          <div class="row">
-            <div class="icon">2</div>
-            <div class="description">
-              <h4>2024</h4>
-              <p>Nuxt, Git, Netlify & Decap CMS</p>
-              <NuxtLink to="blog/2024-01-09-about-this-portfolio/"
-                >KEEP READING >></NuxtLink
-              >
-            </div>
-          </div>
-        </label>
-        <!-- 
-        <input type="radio" name="slide" id="c3" />
-        <label for="c3" class="card">
-          <div class="row">
-            <div class="icon">3</div>
-            <div class="description">
-              <h4>Globalization</h4>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </label>
-
-        <input type="radio" name="slide" id="c4" />
-        <label for="c4" class="card">
-          <div class="row">
-            <div class="icon">4</div>
-            <div class="description">
-              <h4>New technologies</h4>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </label>
-         -->
+        <!-- Hero -->
+        <div class="hero-img">
+          <img src="/img/homepage/ed_placeholder.png" alt="placeholder" />
+        </div>
       </div>
     </div>
   </section>
@@ -85,148 +50,53 @@ if (location.href.indexOf("#invite_token") !== -1) {
 
 <style lang="scss">
 @import "@/assets/scss/partials/colours";
+
+html > body {
+  background-color: #333333;
+}
+
 h1.site-headline {
-  font-size: clamp(2rem, 2.5vw, 3rem);
-  line-height: 1;
-  padding: 0 1rem 1rem 0;
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: clamp(2.58rem, 11vw, 10rem);
+  font-weight: normal;
+  text-transform: uppercase;
+  padding: 0 0;
+  color: #ffffff;
+  text-shadow: 0px 4px 4px #00000025;
   span {
-    color: #7c8497;
-    font-size: clamp(3rem, 4.7vw, 4.2rem);
-    text-shadow: 1px 1px 0px #000;
-    &:nth-of-type(1) {
-      color: black;
+    &:nth-of-type(2) {
+      color: #9b9b9b;
     }
   }
 }
+h2.sub-text ul {
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: clamp(1rem, 2.5vw, 3rem);
+  font-weight: normal;
+  padding: 0 0;
+  color: #ffffff;
+  text-shadow: 0px 4px 4px #00000025;
+  list-style: circle;
+  margin: 0 1rem;
+  padding: 0 0.5rem;
+  line-height: 2rem;
+}
 
-.slide-wrapper {
-  display: flex;
-  flex-wrap: nowrap;
-  flex-direction: column;
-  height: 200px;
-  @media (min-width: 970px) {
-    justify-content: flex-end;
-    flex-direction: row;
+.tech-logos {
+  // text-align: center;
+  padding: 1rem 0;
+  img {
+    margin: 0.25rem;
   }
 }
 
-.card {
-  border-radius: 0.25rem;
-  display: flex;
-  align-items: center;
-  min-width: 91px;
-  width: 91px;
-  padding-right: 0.5rem;
-  overflow: hidden;
-  cursor: pointer;
-  margin: 0rem 0rem 1rem 0;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-color: #fff;
-  transition: 0.3s cubic-bezier(0.28, -0.03, 0, 0.99);
-  box-shadow: 1px 3px 4px #00000020;
-  @media only screen and (min-width: 970px) {
-    align-items: flex-end;
-    margin: 0rem 1rem 0rem 0rem;
-  }
-}
-
-.card > {
-  .row {
-    display: flex;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    .icon {
-      display: block;
-      background: #000000;
-      color: white;
-      border-radius: 50%;
-      min-width: 4.5rem;
-      line-height: 4.5rem;
-      margin: 0.6rem;
-      text-align: center;
-      font-weight: bold;
-      font-size: 1.5rem;
-      transition: 0.25s;
-      &:hover {
-        background: #555;
-      }
-    }
-    .description {
-      height: 80px;
-      width: 100%;
-      opacity: 0;
-      text-decoration: none;
-      transform: translateY(0px);
-      transition: all 0.3s ease;
-      transition-delay: 0.3s;
-      h4 {
-        font-size: clamp(1rem, 1vw, 1.17rem);
-        text-transform: uppercase;
-        padding: 0rem 0 0.2rem;
-        padding: 0.25rem;
-        margin-top: 0.5rem;
-        margin-bottom: 0.25rem;
-        background-color: #0000005c;
-        // color: white;
-        color: #fff767;
-
-        backdrop-filter: blur(5px);
-      }
-      p {
-        font-size: 0.8rem;
-        padding: 0 0.25rem;
-        margin-bottom: 0.25rem;
-        background-color: #0000005c;
-        color: white;
-        backdrop-filter: blur(5px);
-      }
-      a {
-        display: inline-block;
-        font-size: 0.8rem;
-        padding: 0.25rem;
-        margin-right: 1rem;
-        background-color: #0000005c;
-        color: white;
-        text-decoration: none;
-        margin-bottom: 0.25rem;
-        transition: 0.25s;
-        backdrop-filter: blur(5px);
-        &:hover {
-          background-color: #000;
-          color: white;
-        }
-      }
-    }
-  }
-}
-
-input[name="slide"] {
-  display: none;
-}
-
-input:checked + label {
-  /* max-width: 600px; */
-  width: 100%;
-}
-
-input:checked + label .description {
-  opacity: 1 !important;
-  transform: translateY(0) !important;
-}
-
-.card[for="c1"] {
-  background-image: url("/img/homepage/img1.jpg");
-}
-.card[for="c2"] {
-  background-image: url("/img/homepage/img2.jpg");
+.hero-img {
+  text-align: center;
   @media (min-width: 768px) {
+    // position: absolute;
+    position: relative;
+    // left: 0;
+    // z-index: -1;
   }
 }
-// .card[for="c3"] {
-//   background-image: url("./img3.jpeg");
-// }
-// .card[for="c4"] {
-//   background-image: url("./img4.jpeg");
-// }
 </style>
