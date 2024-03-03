904595962c1296cf9467538bd20ea328f482ffcb
Mark Horrocks <mr.horrocks@gmail.com>
2024-03-03 02:46:45 +0000

    blog link removed

--- components/car/nav/Links.vue
+++ components/car/nav/Links.vue
@@ -4,6 +4,6 @@
   <NuxtLink to="" title="Sell your car">Sell your car</NuxtLink>
   <NuxtLink to="" title="Apply for finance">Apply for finance</NuxtLink>
   <NuxtLink to="" title="About us">About us</NuxtLink>
-  <NuxtLink to="" title="Blog">Blog</NuxtLink>
+  <!-- <NuxtLink to="" title="Blog">Blog</NuxtLink> -->
   <NuxtLink to="" title="Contact">Contact</NuxtLink>
 </template>
