55e831b4a9d925d759c4a6f1d3af5af3563c0215
Mark Horrocks <mr.horrocks@gmail.com>
2024-03-05 01:56:45 +0000

    roboto font added to buttton

--- components/car/Button.vue
+++ components/car/Button.vue
@@ -24,6 +24,8 @@ export default {
   padding: 0.5rem 1.5rem;
   margin: 0.2rem 0 0.2rem 0.2rem;
   font-weight: bold;
+  font-family: "Roboto";
+  cursor: pointer;
   &:hover {
     background: darken(#82f7ff, 20%);
   }
