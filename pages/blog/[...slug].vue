2dafb1be72f70693b66269e8d1e09f465caef633
Mark Horrocks <mr.horrocks@gmail.com>
2024-03-07 23:52:55 +0000

    more specific css

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -60,7 +60,7 @@ const { data, error } = await useAsyncData(`content-${cleanPath}`, async () => {
           </header>
 
           <div class="md:grid md:cols-3 md:gap-3">
-            <div class="content">
+            <div class="blog-content">
               <!-- featuredImage -->
               <div v-if="doc.featuredImage" class="featured-image">
                 <img :src="doc.featuredImage" :alt="doc.featuredImageAltText" />
@@ -168,7 +168,7 @@ header.article-header {
   }
 }
 
-div.content {
+div.blog-content {
   background: white;
   padding: 0.5rem;
   box-shadow: 1px 3px 4px #00000020;
