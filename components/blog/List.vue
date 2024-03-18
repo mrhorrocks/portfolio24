09d4cb8a0decb2430872d0c835d0b0ead08609cc
Mark Horrocks <mr.horrocks@gmail.com>
2024-03-18 01:55:58 +0000

    final snags fixed

--- components/blog/List.vue
+++ components/blog/List.vue
@@ -42,28 +42,31 @@ const props = defineProps({
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-
   background-color: white;
   padding: 1.5rem;
   box-sizing: border-box;
-  /* border-radius: 0.75rem; */
+  border-radius: 0.5rem;
   border: transparent;
   box-shadow: 1px 3px 4px #00000020;
-  > img {
-    margin-bottom: 1rem;
+  > a > img {
+    margin-bottom: 0.5rem;
   }
   .blog-index-card-heading {
-    /* margin-top: 1rem; */
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
+    @media (min-width: 1024px) {
+      flex-direction: row;
+    }
     h2 {
-      padding-top: 0;
+      padding: 0 0 0.5rem 0;
     }
   }
   .date {
     font-size: 0.8rem;
     font-style: italic;
     opacity: 0.8;
+    margin: 0 0 1rem 0;
   }
 }
 </style>
